33b116832ad29ac4dffdadbb6eaef343bc2e8f9a
Kageura <kageura30@outlook.com>
2025-03-22 12:02:47 +0900

    Update main.py

--- main.py
+++ main.py
@@ -4,32 +4,32 @@ import json
 import os
 
 
-#if 'money' not in st.session_state:
-    #st.session_state.money = 0
+if 'money' not in st.session_state:
+    st.session_state.money = 0
 
 st.title('Financial Tracker')
 
 description = st.text_input('Enter a description for the item:')
 cost = st.number_input('Enter the cost of the item' )
 earn = st.number_input('Enter how much money you earned:')
-#st.session_state.money -= cost
-#st.session_state.money += earn
+st.session_state.money -= cost
+st.session_state.money += earn
 
-#if 'data' not in st.session_state:  
-    #st.session_state.data = []
+if 'data' not in st.session_state:  
+    st.session_state.data = []
 
 
 def save_to_file():
     file_path = 'financial_data.json'
     with open(file_path, 'w') as f:
-        json.dump(st.session_state.data, f)
+        json.dump(st.session_state, f)
     st.success(f"Data saved to {file_path}")
 
 def load_from_file():
     file_path = 'financial_data.json'
     if os.path.exists(file_path):
         with open(file_path, 'r') as f:
-            st.session_state.data = json.load(f)
+            st.session_state = json.load(f)
         st.success(f"Data loaded from {file_path}")
     else:
         st.warning("No saved data found!")
@@ -37,7 +37,7 @@ def load_from_file():
 
 if st.button('Add Item'):
     if description and (cost or earn):
-        st.session_state.append({'Description': description, 'Cost': cost,}) #'Total Money':st.session_state.money})
+        st.session_state.append({'Description': description, 'Cost': cost, 'Total Money':st.session_state.money})
         st.success('Item added succsesfully')
         save_to_file()
 
