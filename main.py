c812633ad59a166916ffee534db660cdde97f051
Kageura <kageura30@outlook.com>
2025-03-29 12:56:09 +0900

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,8 @@ import pandas as pd
 import json
 import os
 
-
+cost = 0
+earn = 
 # Initialize 'money' and 'data' in session state if they don't exist
 if 'money' not in st.session_state:
     
@@ -62,9 +63,6 @@ if st.button('Load Data'):
 # Button to save data to file
 if st.button('Save Data'):
     save_to_file()
-
-
-st.write(type(st.session_state['money']))
 
 st.write(dict(st.session_state['data']))
 # Display the financial data in a table if it's available
