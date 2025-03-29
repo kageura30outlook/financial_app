c2a7728bfdb7fbbee42efd01bf64b1ab3ffccfc8
Kageura <kageura30@outlook.com>
2025-03-29 12:21:44 +0900

    Update main.py

--- main.py
+++ main.py
@@ -62,5 +62,5 @@ if st.button('Save Data'):
 st.write(dict(st.session_state['data']))
 # Display the financial data in a table if it's available
 if st.session_state['data']:
-    df = pd.DataFrame(dict(st.session_state['data']),index = 0)
+    df = pd.DataFrame(dict(st.session_state['data']).items(),index = 0).T
     st.table(df)
