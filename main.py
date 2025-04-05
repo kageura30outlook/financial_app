abb9257d2ef607ac9546142452544391e0b0fe06
Kageura <kageura30@outlook.com>
2025-04-05 11:06:26 +0900

    Update main.py

--- main.py
+++ main.py
@@ -43,8 +43,7 @@ def load_from_file():
 
 
 if 'data' not in st.session_state:
-    st.session_state['data'] = ()
-
+    st.session_state['data'] = []
 st.write(st.session_state['money'])
 
 # Add item to the financial tracke
