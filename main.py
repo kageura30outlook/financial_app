eea89d95819fc9d9c0e266f190b480cebc2e84b6
Kageura <kageura30@outlook.com>
2025-04-05 12:26:40 +0900

    Update main.py

--- main.py
+++ main.py
@@ -17,22 +17,16 @@ st.title('Financial Tracker')
 description = st.text_input('Enter a description/name for the item:')
 cost = int(st.number_input('Enter the cost of the item'))
 earn = int(st.number_input('Enter how much money you earned:'))
-
-def save_to_file():
-    file_path = 'financial_data.json'
-    with open(file_path, 'w') as f:
-        json.dump(st.session_state['data'], f)
-    st.success(f"Data saved to {file_path}")
-
 # Save the initial value of 'money' before the update
 current_money = st.session_state['money']
 
-# Update the total money based on cost and earn
-st.session_state['money'] -= cost
-st.session_state['money'] += earn
-
 # Add item to the financial tracker
 if st.button('Add Item'):
+
+    st.session_state['money'] -= cost
+    st.session_state['money'] += earn
+
+# Add item to the financial tracker
     if description and (cost or earn):  # Ensure that either cost or earn is entered
         new_item = {
             'Description': description,
